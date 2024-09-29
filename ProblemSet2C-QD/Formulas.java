f5f04fbd543cefb34e89c3440cf00c195347b930
tnt3634 <158097746+tnt3634@users.noreply.github.com>
2024-09-28 23:30:43 -0700

    Add files via upload

--- ProblemSet2C-QD/Formulas.java
+++ ProblemSet2C-QD/Formulas.java
@@ -1,24 +1,27 @@
 
 public class Formulas {
-    
-    
+
     
     public OrderedPair findQuadraticRoots (double a, double b, double c) {
         double xMax = (b * -1 + Math.sqrt(Math.pow(b,2) - (4 * a * c)))/(2*a);
         double xMin = (b * -1 - Math.sqrt(Math.pow(b,2) - (4 * a * c)))/(2*a);
         OrderedPair quadF = new OrderedPair(xMax, xMin);
         return quadF; 
-        
-    }
-    
-    public double findSlope (OrderedPair A, OrderedPair B){
-        
-        OrderedPair slope = new OrderedPair(); 
-        return 1;
-        
+
     }
 
+    public OrderedPair findSlope (double x1, double y1, double x2, double y2){
+        double slopeform = (y2 - y1)/(x2-x1);
+        OrderedPair slope = new OrderedPair(0, slopeform); 
+        return slope;
+
+    }
     
-    
+    public double findArithmeticSeriesSum (double at, double d, int k){
+        //Sum of the first k terms of an arithmetic series: k/2(2at+d( k-1)) 
+        double fas = (double) k/2 * (2 * at +  (k-1) * d);
+        return fas; 
+    }
+
     
 }
--- ProblemSet2C-QD/driver.java
+++ ProblemSet2C-QD/driver.java
@@ -1,33 +1,71 @@
 import java.util.Scanner;
 public class driver{
-    
-    
-    
-    
+
     public static void main(String[] args) {
-    OrderedPair p1, p2; 
-    Scanner input = new Scanner(System.in);
-    
-    Formulas f = new Formulas();
-    p1 = new OrderedPair();
-    p2 = new OrderedPair(1,5);
-    
-    System.out.println("If ordered pair is 0,0"); 
-    System.out.println(p1.toString());
-    System.out.println("X value --> " + p1.getX());
-    System.out.println("Y value --> " + p1.getY());
-    System.out.println(" ");
-    //--------------------------------------------
-    System.out.println("if ordered pair is 1,5");
-    System.out.println(p2.toString());
-    System.out.println("X value --> " + p2.getX());
-    System.out.println("Y value --> " + p2.getY());
-    System.out.println(" ");
-    //--------------------------------------------
-    System.out.println("Finding quadratic of 1x^2 + 2X -3");
-    System.out.println(f.findQuadraticRoots(1,2,-3));
-    System.out.println(" ");
-    
-    
-}
+        OrderedPair p1, p2; 
+        Scanner input = new Scanner(System.in);
+
+        Formulas f = new Formulas();
+        p1 = new OrderedPair();
+        p2 = new OrderedPair(1,5);
+        /*
+        System.out.println("If ordered pair is 0,0"); 
+        System.out.println(p1.toString());
+        System.out.println("X value --> " + p1.getX());
+        System.out.println("Y value --> " + p1.getY());
+        System.out.println(" ");
+        //--------------------------------------------
+        System.out.println("if ordered pair is 1,5");
+        System.out.println(p2.toString());
+        System.out.println("X value --> " + p2.getX());
+        System.out.println("Y value --> " + p2.getY());
+        System.out.println(" ");
+        //--------------------------------------------
+        System.out.println("Finding roots of ax^2 + bX + c");
+        System.out.print("Enter a = ");
+        Double a = input.nextDouble();
+        System.out.print("Enter b = ");
+        Double b = input.nextDouble();
+        System.out.print("Enter c = ");
+        Double c = input.nextDouble();
+        System.out.println(f.findQuadraticRoots(a, b, c));
+        System.out.println(" ");
+        //--------------------------------------------
+
+        System.out.println("Finding slope between x1,y1 and x2, y2");
+        System.out.print("Enter x1 = ");
+        Double x1 = input.nextDouble();
+        System.out.print("Enter x2 = ");
+        Double x2 = input.nextDouble();
+        System.out.print("Enter y1 = ");
+        Double y1 = input.nextDouble();
+        System.out.print("Entefr y2 = ");
+        Double y2 = input.nextDouble();
+        System.out.println("the slope between " + "("+ x1 + "," + y1 + ") and (" + x2 + "," + y2 + ") is " + f.findSlope(x1, x2, y1, y2));
+        System.out.println(" ");
+        //-----------------------------------------------
+
+        
+         
+        SUM OF AN ARITHMETIC SERIES
+        Number of terms: 5
+        Starts with: 1.0
+        Increases by: 1.0
+        The sum of the first 5 terms of an arithmetic series that starts with 1.0 and increases by 1.0 is 15.0
+
+         */
+        System.out.println("Sum of an arithmic series");
+        System.out.print("Enter number of terms = ");
+        Double at = input.nextDouble();
+        System.out.print("Enter starting value = ");
+        Double d = input.nextDouble();
+        System.out.print("Enter Increase amount = ");
+        int k = input.nextInt();
+
+        System.out.println("The sum of the first " + at + " terms of an arithmetic series that starts with " + d + " and increases by " +
+        k + " is " + f.findArithmeticSeriesSum(at, d, k));
+        System.out.println(" ");
+
+       
+    }
 }
