ec27b84f46bea20068bba6b6daaf1eb25f60b1d4
tnt3634 <158097746+tnt3634@users.noreply.github.com>
2024-10-19 22:58:54 -0700

    Add files via upload

--- ProblemSet-4A/StdDraw2.java
+++ ProblemSet-4A/StdDraw2.java
@@ -3,12 +3,12 @@ public class StdDraw2 {
     public static void main (String[] args) {
         StdDraw.setScale(0, 500);
         int x = 0;
-        int y = 0;
+        
         while (x <= 500){
-            StdDraw.line(x, 0, x, 500);
-            StdDraw.line(0, y, 500, y);
+            StdDraw.line(0, x, x, 500);
+            StdDraw.line(x, 0, 500, x);
             x += 20;
-            y += 20;
+          
         }
           
     }
--- ProblemSet-4A/StdDraw3.java
+++ ProblemSet-4A/StdDraw3.java
@@ -1,21 +1,40 @@
 
 public class StdDraw3 {
-	public static void main (String[] args) {
-    	StdDraw.setScale(0, 500);
-   	 
-    	StdDraw.line(0, 500, 500, 0);
-    	StdDraw.line(0, 0, 500, 500);
-    	StdDraw.square(250, 250, 100);
-    	StdDraw.circle(250, 250, 100);
-   	 
-    	StdDraw.setPenColor(StdDraw.RED);
-    	StdDraw.filledCircle(100, 100, 50);
-    	StdDraw.setPenColor(StdDraw.GREEN);
-    	StdDraw.filledCircle(100, 400, 50);
-    	StdDraw.setPenColor(StdDraw.BLUE);
-    	StdDraw.filledCircle(400, 400, 50);
-    	                         
-      StdDraw.setPenColor(StdDraw.YELLOW);
-    	StdDraw.filledCircle(400, 100, 50);
-	}
+    public static void main (String[] args) {
+        
+        StdDraw.setScale(0, 500);
+        int x = 250;
+        int y = 10;
+        int z = 250;
+        while (x <= 500 && y <= 500){
+	    if (y % 20 == 0){
+	    StdDraw.setPenColor(StdDraw.WHITE); 
+	    }else {
+	    StdDraw.setPenColor(StdDraw.BLACK);  
+	    }
+            StdDraw.circle(x, z, y); 
+            //z += 2;
+            //x += 2;
+            y += 20;
+        }
+	    
+        /*    
+        StdDraw.setScale(0, 500);
+
+        StdDraw.line(0, 500, 500, 0);
+        StdDraw.line(0, 0, 500, 500);
+        StdDraw.square(250, 250, 100);
+        StdDraw.circle(250, 250, 100);
+
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.filledCircle(100, 100, 50);
+        StdDraw.setPenColor(StdDraw.GREEN);
+        StdDraw.filledCircle(100, 400, 50);
+        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.filledCircle(400, 400, 50);
+
+        StdDraw.setPenColor(StdDraw.YELLOW);
+        StdDraw.filledCircle(400, 100, 50);
+         */
+    }
 }
