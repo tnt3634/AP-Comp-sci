bd6a1ef9d0a646a87a356382b726137b03c3c02b
tnt3634 <158097746+tnt3634@users.noreply.github.com>
2024-10-28 19:32:17 -0700

    Update StdChaosGame.java
    The save wouldnt upload changes

--- ProblemSet-4A/StdChaosGame.java
+++ ProblemSet-4A/StdChaosGame.java
@@ -3,8 +3,10 @@ public class StdChaosGame {
     public static void main (String[] args) {
 
         OrderedPair p1, p2, p3;
-        int x = 0;
-        int y = 0;
+        int rp = 0;
+        double x = 0;
+        double y = 1;
+        int i = 0;
         StdDraw.setScale(0, 1);
         
         StdDraw.setPenRadius(0.02);
@@ -16,7 +18,28 @@ public class StdChaosGame {
         StdDraw.point(p2.getX(), p2.getY());
         StdDraw.point(p3.getX(), p3.getY());
         
+        while (i <= 10000){
+            rp = (int) (Math.random()*3) + 1 ;   
 
+            if  (rp == 1){
+                x = (x + p1.getX())/2;
+                y = (y + p1.getY())/2;
+              
+            }else if (rp == 2){
+
+                x = (x + p2.getX())/2;
+                y = (y + p2.getY())/2;
+                
+
+            }else {
+
+                x = (x + p3.getX())/2;
+                y = (y + p3.getY())/2;
+               
+            }
+            StdDraw.point(x,y);
+            i++;
+        }
         
     }
 }
