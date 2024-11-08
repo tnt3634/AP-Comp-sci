2bd4ac22bff80c76c3946a7de2cc6f15259a889b
tnt3634 <158097746+tnt3634@users.noreply.github.com>
2024-11-08 12:22:25 -0800

    Update Driver.java

--- PS4C/Driver.java
+++ PS4C/Driver.java
@@ -6,26 +6,11 @@
  * @version (a version number or a date)
  */
 public class Driver{
-    /*
-    public static void main (String[] args) {
-    String a = ""; 
-    a = Review.textToString("SimpleReview.txt");
-    a = Review.removePunctuation(a);
-
-    while (a.indexOf(" ") >= 0){
-
-    int fsi = a.indexOf(" ");    
-    String word = a.substring(0, fsi);
-    a = a.substring(fsi + 1); 
-
-    }
-
-    System.out.print(a);  
-
-    }
-     */
+   
 
     public static void main (String[] args) {
-        System.out.println(Review.fakeReview("SimpleReview.txt"));
+        System.out.println(Review.textToString("positiveReview.txt")); 
+        System.out.println("Owners response: ");
+        System.out.println(Review.fakeReview("PositiveReview.txt"));
     }
 }
